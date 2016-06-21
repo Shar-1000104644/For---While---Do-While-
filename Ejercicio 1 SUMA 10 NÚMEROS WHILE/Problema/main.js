a11cad718ff95479142c459e2f7e283d5f6ea503
Rosario Rosales Calle <blu-24c_@hotmail.com>
2016-06-20 23:59:26 -0500

    Zzz! Zombi :3

--- Ejercicio 1 SUMA 10 NÚMEROS WHILE/Problema/main.js
+++ Ejercicio 1 SUMA 10 NÚMEROS WHILE/Problema/main.js
@@ -1,1 +1,59 @@
 //Escribe aquí tú código
+
+// Se requiere un algoritmo para obtener la suma de diez 
+// cantidades mediante la utilización de un ciclo “WHILE”.
+
+// function suma() {
+//     var n =document.getElementById('dato').value;
+//     var i = 1;
+//     while (i < 11) {
+//         n += "<br>The number is " + i;
+//         i++;
+//     }
+//     document.getElementById("demo").innerHTML = n;
+// }
+
+
+// function suma() {
+// var resultado;
+// var n = document.getElementById('dato').value;
+// var i = 0;
+ 
+// while(i <= n) {
+//   resultado += i;
+//   i++;
+
+//     }
+//     document.getElementById("demo").innerHTML = resultado;
+// }
+
+
+
+// var numero=1;
+// while(numero<11){
+// 	alert(numero);
+// 	numero++
+// }
+
+
+ 
+// var z=0;
+// do{
+// 	alert(z);
+// 	z=z+1;
+// }while(z<10);
+
+// for (var i = 0; i <= 10; i++) {
+// 	alert(i);
+// }
+
+var resultado = 0;
+var numero = 5;
+var i = 0;
+ 
+while(i <= numero) {
+  resultado += i;
+  i++;
+}
+ 
+alert(resultado);
